a3c8fa2ee2eeb5dc55a5859dbca47975b545ad63
grgp <g.a.pitoy@gmail.com>
2023-04-18 15:40:44 +0700

    Not working vector layers

--- src/Map/BasicMap.tsx
+++ src/Map/BasicMap.tsx
@@ -4,8 +4,14 @@ import { Map, View } from 'ol';
 import { RealtimeLayer, MaplibreLayer } from 'mobility-toolbox-js/ol';
 import 'ol/ol.css';
 import { fromLonLat } from 'ol/proj';
-import { fetchBoundaries, fetchJakarta } from './fetches';
+// import { fetchBoundaries, fetchJakarta } from './fetches';
 // import { villages } from '../data/villages-with-boundaries-600';
+import { villages } from '../data/villages-with-boundaries-265-20chunks.json';
+
+import GeoJSON from 'ol/format/GeoJSON.js';
+import VectorSource from 'ol/source/Vector';
+import VectorLayer from 'ol/layer/Vector';
+import { Style, Stroke, Fill } from 'ol/style';
 
 export function BasicMap() {
   // const [villages, setVillages] = React.useState([]);
@@ -14,9 +20,9 @@ export function BasicMap() {
     // fetchBoundaries().then((data) => {
     //   console.log('what is data?', data);
     // });
-    fetchJakarta().then((data) => {
-      console.log('what is jakarta data?', data);
-    });
+    // fetchJakarta().then((data) => {
+    //   console.log('what is jakarta data?', data);
+    // });
   });
 
   useEffect(() => {
@@ -45,6 +51,59 @@ export function BasicMap() {
     });
     layer.attachToMap(map);
 
+    const geoJsonFeatures = villages.map((village) => {
+      const geometry = {
+        type: 'Polygon',
+        coordinates: [
+          village.members
+            .filter((member) => member.role === 'outer')
+            .map((member) =>
+              member.geometry.map((point) => [point.lon, point.lat])
+            )
+        ]
+      };
+
+      console.log('geometry', { geometry });
+
+      return {
+        type: 'Feature',
+        geometry,
+        properties: village.tags
+      };
+    });
+
+    console.log('what are geojson features', { geoJsonFeatures });
+
+    const geoJson = {
+      type: 'FeatureCollection',
+      features: geoJsonFeatures
+    };
+
+    // Create a vector source from the GeoJSON data
+    const vectorSource = new VectorSource({
+      features: new GeoJSON().readFeatures(geoJson, {
+        dataProjection: 'EPSG:4326',
+        featureProjection: 'EPSG:4326'
+      })
+    });
+
+    // Create a vector layer with a simple stroke style
+    const vectorLayer = new VectorLayer({
+      source: vectorSource,
+      style: new Style({
+        stroke: new Stroke({
+          color: 'blue',
+          width: 2
+        }),
+        fill: new Fill({
+          color: '#ff9090'
+        })
+      })
+    });
+
+    // Add the vector layer to the map
+    map.addLayer(vectorLayer);
+
     // const tracker = new RealtimeLayer({
     //   url: 'wss://api.geops.io/tracker-ws/v1/',
     //   apiKey: window.apiKey,
@@ -67,7 +126,9 @@ export function BasicMap() {
   return (
     <div>
       <h1>Basic Map</h1>
-      {/* <textarea>{JSON.stringify(villages.map(village => village.tags.name))}</textarea> */}
+      <textarea>
+        {JSON.stringify(villages.map((village) => village.tags.name))}
+      </textarea>
       <div id="map" style={{ width: 500, height: 500 }}></div>
     </div>
   );
