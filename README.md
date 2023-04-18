08e7273c689ea861cf1fd3a174d2552e923be764
George Pitoy <g.a.pitoy@gmail.com>
2023-04-18 12:00:11 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# vite React TypeScript tailwindcss starter
+
+Template for vite, React + tailwindcss + TypeScript projects with some tools preconfigured.
+
+## About
+
+Template for vite, React + tailwindcss + TypeScript projects with some tools preconfigured.
+
+![Screenshot](assets/screenshot.png)
+
+### Libraries
+
+- [Jest 29](https://jestjs.io/)
+- [React 18](https://reactjs.org/)
+- [tailwindcss 3](https://tailwindcss.com/)
+  - [tailwindcss forms plugin](https://tailwindcss-forms.vercel.app/)
+- [TypeScript 4.9.5](https://www.typescriptlang.org/)
+- [vite 4](https://vitejs.dev/)
+
+### Tools
+
+- [commitlint 17](https://commitlint.js.org)
+- [Conventional Commits 1](https://www.conventionalcommits.org)
+- [editorconfig](https://editorconfig.org/)
+- [eslint 8](https://eslint.org/)
+- [husky 8](https://typicode.github.io/husky/#/)
+- [Prettier 2](https://prettier.io/)
+- [VS Code settings](https://code.visualstudio.com/)
+
+## Usage
+
+Create a new project from this template by clicking the "Use this template" button:
+
+![](assets/template-usage.png)
+
+```bash
+yarn
+yarn dev
+yarn test
+```
---
+++ src/App.tsx
@@ -0,0 +1,70 @@
+import React, { ReactElement, useState } from 'react'
+import logo from './logo.svg'
+import viteLogo from './vite.svg'
+import tailwindLogo from './tailwind.svg'
+
+function App(): ReactElement {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="p-20 border shadow-xl border-gray-50 rounded-xl">
+      <header>
+        <div className="flex justify-center">
+          <img src={viteLogo} className="w-32 h-32" alt="vite logo" />
+          <img src={logo} className="w-32 h-32" alt="React logo" />
+          <img
+            src={tailwindLogo}
+            className="w-32 h-32"
+            alt="Tailwind CSS logo"
+          />
+        </div>
+        <p className="pb-3 text-2xl">Hello Vite + React + Tailwind CSS!</p>
+        <p>
+          <button
+            className="pt-1 pb-1 pl-2 pr-2 text-sm text-purple-100 bg-purple-400 rounded"
+            onClick={() => setCount((count) => count + 1)}
+          >
+            count is: {count}
+          </button>
+        </p>
+        <p className="pt-3 pb-3">
+          Edit{' '}
+          <code className="border border-1 pl-1 pr-1 pb-0.5 pt-0.5 rounded border-purple-400 font-mono text-sm bg-purple-100 text-purple-900">
+            src/App.tsx
+          </code>{' '}
+          and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="text-purple-400 underline"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="text-purple-400 underline"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+          {' | '}
+          <a
+            className="text-purple-400 underline"
+            href="https://tailwindcss.com/docs"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Tailwind CSS Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
