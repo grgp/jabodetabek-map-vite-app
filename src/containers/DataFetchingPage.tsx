5f756ae226362ea3d0dd7500c9ecdeb2f9595128
grgp <g.a.pitoy@gmail.com>
2023-04-19 15:26:02 +0700

    DataFetchingPage

--- src/containers/DataFetchingPage.tsx
+++ src/containers/DataFetchingPage.tsx
@@ -1,9 +1,31 @@
 import * as React from 'react';
+import { fetchJakarta } from '../map/fetches';
+
+const buttonStyle =
+  'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded';
 
 export function DataFetchingPage() {
+  const [isFetchJakartaLoading, setIsFetchJakartaLoading] =
+    React.useState(false);
+
   return (
-    <div>
-      <p>hello</p>
+    <div className="flex flex-col gap-4">
+      <button
+        className={buttonStyle}
+        onClick={() => {
+          setIsFetchJakartaLoading(true);
+
+          fetchJakarta().then((data) => {
+            setIsFetchJakartaLoading(false);
+            console.log('fetchJakarta done', data);
+          });
+        }}
+      >
+        {isFetchJakartaLoading ? 'Loading...' : 'Fetch Jakarta'}
+      </button>
+      <button className={buttonStyle} onClick={() => {}}>
+        Fetch Data 2
+      </button>
     </div>
   );
 }
