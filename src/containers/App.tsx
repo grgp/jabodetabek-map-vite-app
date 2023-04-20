adc041808e651955adf92bc5bf5e377ec7ca9fb6
grgp <g.a.pitoy@gmail.com>
2023-04-21 00:56:06 +0700

    Fix with overflow hidden

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -7,7 +7,7 @@ function App(): ReactElement {
   return (
     <div style={{ width: '100%' }}>
       <div className="flex flex-col md:flex-row h-screen">
-        <div className="flex-1 h-full bg-gray-300 relative">
+        <div className="flex-1 h-full bg-gray-300 relative overflow-hidden">
           <MainMap />
         </div>
 
