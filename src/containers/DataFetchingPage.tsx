a584604502783529d7eb50a70eea86a62c9ed422
grgp <g.a.pitoy@gmail.com>
2023-04-21 01:52:20 +0700

    Trains passing jakarta data

--- src/containers/DataFetchingPage.tsx
+++ src/containers/DataFetchingPage.tsx
@@ -3,6 +3,9 @@ import { uniqBy } from 'lodash';
 import { fetchJakarta } from '../map/fetches';
 import { Village } from '../types/structure';
 import CSVUploader from './CSVUploader';
+import { Train } from '../types/unparsedStructures';
+
+import trainsInJakarta from '../data/trains/trains-passing-jakarta.jsonc';
 
 const buttonStyle =
   'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded';
@@ -43,8 +46,27 @@ export function DataFetchingPage() {
         {isFetchJakartaLoading ? 'Loading...' : 'Fetch Jakarta'}
       </button>
 
-      <button className={buttonStyle} onClick={() => {}}>
-        Fetch Data 2
+      <button
+        className={buttonStyle}
+        onClick={() => {
+          // Get unique trains
+
+          async function fetchTrainSchedules(trains: Train[]): Promise<void> {
+            const batchSize = 3;
+            for (let i = 0; i < trains.length; i += batchSize) {
+              const batch = trains.slice(i, i + batchSize);
+              for (const train of batch) {
+                const url = `https://api-partner.krl.co.id/krlweb/v1/schedule-train?trainid=${train.train_id}`;
+                const response = await axios.get(url);
+                // Do something with the response data, e.g. log it
+                console.log(response.data);
+              }
+              await new Promise((resolve) => setTimeout(resolve, 5000)); // Wait 5 seconds before processing the next batch
+            }
+          }
+        }}
+      >
+        Get Unique Trains in Jakarta
       </button>
 
       <CSVUploader />
---
+++ src/types/unparsedStructures.ts
@@ -0,0 +1,9 @@
+export interface Train {
+  train_id: string;
+  ka_name: string;
+  route_name: string;
+  dest: string;
+  time_est: string;
+  color: string;
+  dest_time: string;
+}
